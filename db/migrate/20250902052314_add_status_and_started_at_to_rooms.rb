e18d11a4873fc6a50cf58cc88e9f68bb95e15d92
lala <226228442+lala8639@users.noreply.github.com>
2025-09-02 15:50:47 +0900

    マイグレーションファイルの修正

--- db/migrate/20250902052314_add_status_and_started_at_to_rooms.rb
+++ db/migrate/20250902052314_add_status_and_started_at_to_rooms.rb
@@ -1,18 +1,7 @@
 class AddStatusAndStartedAtToRooms < ActiveRecord::Migration[8.0]
-  # DDLトランザクションを無効化する
-  disable_ddl_transaction!
-
-  def up
+  def change
     add_column :rooms, :status, :integer, default: 0, null: false
     add_column :rooms, :started_at, :datetime
-    # algorithm: :concurrently を指定してインデックスを非同期で作成
-    add_index :rooms, :status, algorithm: :concurrently
-  end
-
-  def down
-    # upメソッドで追加したものを逆順で削除していく
-    remove_index :rooms, :status
-    remove_column :rooms, :started_at
-    remove_column :rooms, :status
+    add_index :rooms, :status
   end
 end
