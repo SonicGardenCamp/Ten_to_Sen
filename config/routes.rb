bda691c6cb1b0e5c11b0519f5fda92e111f55817
佐藤瑞起 <117ema3cia@gmail.com>
2025-09-02 14:33:22 +0900

    ログイン・ログアウトできるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'rooms#index'
+
+  devise_scope :user do
+    # 未ログイン時の root はログイン画面
+    root to: 'devise/sessions#new'
+  end
 
   resources :rooms, only: %i[index show create] do
     get :result, on: :member # この行を追加
