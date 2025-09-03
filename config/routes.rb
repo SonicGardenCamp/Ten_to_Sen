a4575e3bc8af2ca91cbbb1e479e8352dcbfc902d
lala <226228442+lala8639@users.noreply.github.com>
2025-09-03 23:09:07 +0900

    ルートの改修ver2

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  #認証済みか、認証済みでないかで2種類のrootのどちらかを出す
-  authenticated :user do  #認証済みのユーザー
+  # 認証済みユーザーのルートパス
+  authenticated :user do
     root to: 'rooms#index', as: :authenticated_root
   end
 
   devise_scope :user do
-    unauthenticated do  #認証済みでないユーザー
+    unauthenticated do
       root to: 'devise/sessions#new', as: :unauthenticated_root
     end
   end
-
+  
   resources :rooms, only: %i[index show create new] do
     get :result, on: :member
-    get :new
   end
+  
   resources :words, only: [:create]
 end
