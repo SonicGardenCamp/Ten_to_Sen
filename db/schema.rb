b51fd644dfe8867a15e49a6b90083cda90d1a33a
rag <rag@tuD.>
2025-09-03 14:37:30 +0900

    ルーム作成画面(rooms/new)の見た目だけを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_02_020612) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_02_055954) do
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,6 +26,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_02_020612) do
   create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "theme"
+    t.string "game_mode"
+    t.string "password_digest"
   end
 
   create_table "users", force: :cascade do |t|
