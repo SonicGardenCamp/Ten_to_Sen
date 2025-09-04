0bb9380ec95fc3800155e5a9ebbc837e358cb442
Hiroshi Ueki <ueki@sonicgarden.jp>
2025-09-04 18:08:05 +0900

    geminiのconfig修正

--- config/initializers/gemini.rb
+++ config/initializers/gemini.rb
@@ -1,6 +1,6 @@
 require 'gemini_craft'
 
 GeminiCraft.configure do |config|
-  config.api_key = Rails.application.credentials.google[:gemini_api_key]
+  config.api_key = Rails.application.credentials.dig(:google, :gemini_api_key)
   config.logger = Rails.logger
 end
